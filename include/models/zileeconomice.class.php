f140d5a3f5c4bbfd62efcaa2a782146ddff4e309
Eduard Budacu <eduard.budacu@gmail.com>
2024-10-28 18:51:37 +0100

    fix bug zi economica

--- include/models/zileeconomice.class.php
+++ include/models/zileeconomice.class.php
@@ -1,38 +1,42 @@
 <?php
 class ZileEconomice extends AbstractDB
 {
-	var $useTable = "zile_economice";
-	var $primaryKey = "zi_economica_id";
-	function __construct($mysql,$id=NULL)
-		{
-		parent::__construct($mysql, $id);
-		}
-	
-	function getLastDay()
-		{
-		$this -> findLast(array("WHERE", "inchis" => " = 'NU'"));
-		}
-	
-	function closeDay($user_id)
-		{
-		$this -> getLastDay();
-		$closed = $this -> obj;
-		$this -> setObjValue("inchis", "DA");
-		$this -> setObjValue("ora_inchidere", date("H:i:s"));
-		$this -> setObjValue("user_id", $user_id);
-		$this -> save();
-		$this -> resetObj();
-		$this -> setObjId(0);
-if($closed -> data == '2012-10-28') {
-$this -> setObjValue("data", '2012-10-29');
+    var $useTable = "zile_economice";
+    var $primaryKey = "zi_economica_id";
 
-} else {
-		$this -> setObjValue("data", date("Y-m-d", strtotime($closed -> data) + 3600*24));
-}
-		$this -> setObjValue("inchis", "NU");
-		$this -> save();
-		$this -> getLastDay();
-		return $closed;
-		}		
+    function __construct($mysql, $id = NULL)
+    {
+        parent::__construct($mysql, $id);
+    }
+
+    function getLastDay()
+    {
+        $this->findLast(array("WHERE", "inchis" => " = 'NU'"));
+    }
+
+    function closeDay($user_id)
+    {
+        $this->getLastDay();
+        $closed = $this->obj;
+        
+        $this->setObjValue("inchis", "DA");
+        $this->setObjValue("ora_inchidere", date("H:i:s"));
+        $this->setObjValue("user_id", $user_id);
+        $this->save();
+
+        $this->resetObj();
+        $this->setObjId(0);
+
+        // Folosim DateTime pentru a adăuga o zi și a evita problemele de fus orar
+        $date = new DateTime($closed->data);
+        $date->modify('+1 day');
+        $this->setObjValue("data", $date->format('Y-m-d'));
+
+        $this->setObjValue("inchis", "NU");
+        $this->save();
+
+        $this->getLastDay();
+        return $closed;
+    }
 }
 ?>
